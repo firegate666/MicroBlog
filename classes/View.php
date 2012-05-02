80a8d36aff09b742b680e9ab8d309c091bd85639
Marco Behnke <marco@behnke.biz>
2012-05-02 22:58:25 +0200

    reformat

--- classes/View.php
+++ classes/View.php
@@ -2,11 +2,13 @@
 
 class View
 {
+
 	public $template;
-	
-	public function render($layout, $parameters = array()) {
+
+	public function render($layout, $parameters = array())
+	{
 		extract($parameters);
-		$content = include __DIR__ . '/../templates/'.$layout.'.php';
+		$content = include __DIR__ . '/../templates/' . $layout . '.php';
 		return $content;
 	}
 }
--- index.php
+++ index.php
@@ -12,16 +12,19 @@ spl_autoload_register(function ($class_name)
 });
 
 $controller_name = 'BlogController'; // default
-if (!empty($_GET['controller'])) {
+if (! empty($_GET['controller']))
+{
 	$controller_name = ucfirst($_GET['controller']) . 'Controller';
 }
 
 $action_name = 'actionList';
-if (!empty($_GET['action'])) {
-	$action_name = 'action'.ucfirst($_GET['action']);
+if (! empty($_GET['action']))
+{
+	$action_name = 'action' . ucfirst($_GET['action']);
 }
 
-if (!is_callable(array($controller_name, $action_name))) {
+if (! is_callable(array($controller_name, $action_name)))
+{
 	throw new LogicException('requested controller or action is invalid', 404);
 }
 
