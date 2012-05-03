f34041cd7f9d7b64ece694848349d11d4b4bb626
Marco Behnke <marco@behnke.biz>
2012-05-03 08:49:38 +0200

    render blog layout

--- classes/BlogController.php
+++ classes/BlogController.php
@@ -6,9 +6,9 @@ class BlogController
 	public function actionList()
 	{
 		$view = new View();
-		
+
 		$result = new HTMLResult();
-		$result->result = $view->render('main', array('title' => 'Dummy'));
+		$result->result = $view->render('blog', array('title' => 'Dummy'));
 		return $result;
 	}
 
