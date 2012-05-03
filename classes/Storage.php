ce22376da83556f2c8a930b6eef85a0abe0db816
Marco Behnke <marco@behnke.biz>
2012-05-03 08:27:05 +0200

    add find to storage

--- classes/Storage.php
+++ classes/Storage.php
@@ -7,6 +7,12 @@ abstract class Storage
 
 	public abstract function load(Model $empty_model);
 
+	public abstract function find(Model $empty_model, $attributes = array(), $order = array());
+
+	public function findAll(Model $empty_model, $order = array()) {
+		return $this->find($empty_model, array(), $order);
+	}
+
 	public abstract function connect($identifier);
 
 	public abstract function disconnet();
