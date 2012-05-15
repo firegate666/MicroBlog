2c6a31615787f44e530376e6da636e6b7d7c7e5f
Marco Behnke <marco@behnke.biz>
2012-05-15 11:47:50 +0200

    whitespace

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,7 +8,7 @@ if (! empty($_GET['controller']))
 	$controller_name = $config->getConfigSub('general', 'default_controller_namespace').$controller_name;
 }
 
-if (! class_exists($controller_name))
+if (!class_exists($controller_name))
 {
 	throw new LogicException('requested controller is invalid', 404);
 }
