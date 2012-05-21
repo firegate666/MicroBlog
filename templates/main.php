02432b488dd6d04f20e70345c3c7dc9301f7bb6e
Marco Behnke <marco@behnke.biz>
2012-05-21 22:05:58 +0200

    fix html

--- templates/main.php
+++ templates/main.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
-	<title>Micro | Blog</title>
-	<link rel="stylesheet" type="text/css" href="css/reset.css" />
-	<link rel="stylesheet" type="text/css" href="css/main.css" />
+	<head>
+		<title>Micro | Blog</title>
+		<link rel="stylesheet" type="text/css" href="css/reset.css" />
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
+	</head>
 <body>
 
 	<div id="content">
