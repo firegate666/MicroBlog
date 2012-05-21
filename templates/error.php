4206a56075a8ccc1bdc09ff8ddbd8076431d31dd
Marco Behnke <marco@behnke.biz>
2012-05-21 23:07:37 +0200

    add error styles

--- templates/error.php
+++ templates/error.php
@@ -1,2 +1,4 @@
-<h1><?=$exception->getMessage()?></h1>
-<pre><?=$exception->getTraceAsString()?></pre>
+<div id="error-content">
+	<h1><?=$exception->getMessage()?></h1>
+	<pre><?=$exception->getTraceAsString()?></pre>
+</div>
