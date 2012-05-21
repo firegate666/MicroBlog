6ec612ef138e67810df6d0794728f5a969c3a0d0
Marco Behnke <marco@behnke.biz>
2012-05-21 23:23:08 +0200

    add jquery

--- templates/main.php
+++ templates/main.php
@@ -4,6 +4,14 @@
 		<title>Micro | Blog</title>
 		<link rel="stylesheet" type="text/css" href="css/reset.css" />
 		<link rel="stylesheet" type="text/css" href="css/main.css" />
+
+		<script type="text/css"
+			<?php if (defined('APP_ENV_DEBUG') && APP_ENV_DEBUG): ?>
+				src="http://code.jquery.com/jquery-1.7.2.js"
+			<?php else: ?>
+				src="http://code.jquery.com/jquery-1.7.2.min.js"
+			<?php endif; ?>
+		></script>
 	</head>
 <body>
 
