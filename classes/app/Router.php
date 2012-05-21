d644d6a948dfb1304b0570ac386fd0982fe2dc5d
Marco Behnke <marco@behnke.biz>
2012-05-21 21:52:23 +0200

    add error renderer to app router to render errors with custom layout if possible

--- classes/app/Router.php
+++ classes/app/Router.php
@@ -2,8 +2,10 @@
 
 namespace app;
 
+use \helper\HTMLResult;
 use \helper\Request;
 use \helper\ApplicationConfig;
+use \rendering\View;
 
 /**
  * handle routing for application
@@ -27,6 +29,33 @@ class Router
 	}
 
 	/**
+	 * use view renderer to display error page
+	 * if view is null, exception is dispatched to registered exception handler
+	 *
+	 * @param \Exception $exception
+	 * @param View $renderer
+	 * @throws Exception thrown if renderer is null
+	 * @return \helper\Result
+	 */
+	public function renderError(\Exception $exception, View $renderer = null)
+	{
+		if ($renderer !== null)
+		{
+			$result = $renderer->render($this->config->getSectionEntry('rendering', 'error_layout'),
+				array(
+					'exception' => $exception,
+				)
+			);
+			return new HTMLResult($result);
+		}
+		else
+		{
+			// dispatch to registered exception handler
+			throw $exception;
+		}
+	}
+
+	/**
 	 *
 	 * @param Request $request
 	 * @throws \LogicException
@@ -34,20 +63,31 @@ class Router
 	 */
 	public function run(Request $request)
 	{
-		$controller_name = $this->config->getSectionEntry('general', 'default_controller'); // default
-		if (! empty($_GET['controller']))
-		{
-			$controller_name = ucfirst($request->getOrPostParam('controller')) . 'Controller';
-			$controller_name = $this->config->getSectionEntry('general', 'default_controller_namespace') . $controller_name;
-		}
-
-		if (! class_exists($controller_name))
-		{
-			throw new \LogicException('requested controller is invalid', 404);
-		}
-
 		$rendering_class = $this->config->getSectionEntry('rendering', 'class');
-		$controller = new $controller_name($this->config, new $rendering_class($this->config->getSection('rendering')));
-		return $controller->handle($request);
+		try {
+			if (! class_exists($rendering_class))
+			{
+				throw new \LogicException('configured renderer is invalid', 404);
+			}
+
+			$controller_name = $this->config->getSectionEntry('general', 'default_controller'); // default
+			if (! empty($_GET['controller']))
+			{
+				$controller_name = ucfirst($request->getOrPostParam('controller')) . 'Controller';
+				$controller_name = $this->config->getSectionEntry('general', 'default_controller_namespace') . $controller_name;
+			}
+
+			if (! class_exists($controller_name))
+			{
+				throw new \LogicException('requested controller is invalid', 404);
+			}
+
+			$controller = new $controller_name($this->config, new $rendering_class($this->config->getSection('rendering')));
+			return $controller->handle($request);
+		}
+		catch (\Exception $e)
+		{
+			return $this->renderError($e, new $rendering_class($this->config->getSection('rendering')));
+		}
 	}
 }
