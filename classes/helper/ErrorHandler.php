aceee8e1e70dd7ee99f5233198a5b9d8afb33708
Marco Behnke <marco@behnke.biz>
2012-05-22 08:57:07 +0200

    throw error exception instead of runtime exception

--- classes/helper/ErrorHandler.php
+++ classes/helper/ErrorHandler.php
@@ -6,17 +6,18 @@ class ErrorHandler
 {
 
 	/**
-	 * convert php error to RuntimeException
+	 * convert php error to ErrorException
 	 *
 	 * @param integer $errno
 	 * @param string $errstr
 	 * @param string $errfile
 	 * @param integer $errline
 	 * @param array $errcontext
+	 * @throws \ErrorException
 	 * @return void
 	 */
-	function handle($errno, $errstr, $errfile, $errline, $errcontext)
+	function handle($errno, $errstr, $errfile, $errline)
 	{
-		throw new \RuntimeException($errstr, $errno);
+		throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
 	}
 }
