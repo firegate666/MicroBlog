5a97a73ac74f2ead3f227f5176c290c036d2a3b5
Marco Behnke <marco@behnke.biz>
2013-11-23 16:03:55 +0100

    add RenderingInterface definition

--- classes/rendering/InvalidRendererClassException.php
+++ classes/rendering/InvalidRendererClassException.php
@@ -1,14 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: marcobehnke
- * Date: 23.11.13
- * Time: 15:58
- */
 
 namespace rendering;
 
-
-class InvalidRendererClassException {
+class InvalidRendererClassException extends \Exception {
 
 }
--- classes/rendering/RenderingInterface.php
+++ classes/rendering/RenderingInterface.php
@@ -1,14 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: marcobehnke
- * Date: 23.11.13
- * Time: 15:52
- */
 
 namespace rendering;
 
+interface RenderingInterface {
 
-class RenderingInterface {
+	/**
+	 * load layout and render with given parameters
+	 *
+	 * @param string $layout
+	 * @param array $parameters
+	 * @param string $wrap
+	 * @throws \InvalidArgumentException if layout is not found
+	 * @return string
+	 */
+	public function render($layout, $parameters = array(), $wrap = 'main');
+
+	/**
+	 * render layout without wrap
+	 *
+	 * @uses View::render()
+	 * @param string $layout
+	 * @param array $parameters
+	 * @return string
+	 */
+	public function renderPartial($layout, $parameters);
 
 }
