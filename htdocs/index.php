bf3e722c3c15fa831c5bd7583b8ebe2c12ee6d6e
Marco Behnke <marco@behnke.biz>
2013-11-24 10:32:12 +0100

    remove condition for result, always compatible now

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,15 +8,16 @@ require_once __DIR__ . '/../setup.php';
 $request = new \helper\Request($_SERVER, $_GET, $_POST);
 
 $app_router_class = $config->getSectionEntry('general', 'default_app_router');
+
+/** @var \app\RouterInterface $app_router */
 $app_router = new $app_router_class($config, $logger);
+
+/** @var \helper\RequestResult $result */
 $result = $app_router->run($request);
 
-if ($result instanceof \helper\HTMLResult || $result instanceof \helper\JSONResult)
-{
-	if ($config->getSectionEntry('output', 'gzip', false))
-	{
-		ob_start('ob_gzhandler');
-	}
+if ($config->getSectionEntry('output', 'gzip', false)) {
+	ob_start('ob_gzhandler');
+}
 
 foreach ($result->getHeaders() as $header) {
 	header($header, true, $result->getHttpStatus());
