d06a0a1d8fec1c95d0f760695d4d39242adbb37b
Marco Behnke <marco@behnke.biz>
2013-11-24 22:47:29 +0100

    add missing references

--- install/install_mysql.sql
+++ install/install_mysql.sql
@@ -1,55 +1,25 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Erstellungszeit: 24. Nov 2013 um 22:05
--- Server Version: 5.6.12
--- PHP-Version: 5.5.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Datenbank: `microblog`
---
-CREATE DATABASE IF NOT EXISTS `microblog` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `microblog`;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `Blog`
---
-
 CREATE TABLE IF NOT EXISTS `Blog` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `Comment`
---
-
-CREATE TABLE IF NOT EXISTS `Comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `postId` int(11) NOT NULL,
-  `content` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `Post`
---
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Post` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `blogId` int(11) NOT NULL,
   `content` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `Comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `postId` int(11) NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+ALTER TABLE `Comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`postId`) REFERENCES `Post` (`id`);
+
+ALTER TABLE `Post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`blogId`) REFERENCES `Blog` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
