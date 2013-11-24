a2ce0779d7ab48415beecc2a95b3776e70dbe663
Marco Behnke <marco@behnke.biz>
2013-11-24 23:50:26 +0100

    add comparator for posts collection

--- htdocs/js/collections/posts.collection.js
+++ htdocs/js/collections/posts.collection.js
@@ -6,7 +6,10 @@
 
 	app.Posts = Backbone.Collection.extend({
 		model: app.Post,
-		url: '?controller=Post&action=list'
+		url: '?controller=Post&action=list',
+		comparator : function(a, b) {
+			return a.getId() > b.getId() ? -1 : 1;
+		}
 	});
 
 }(window, Backbone));
