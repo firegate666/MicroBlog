01b9bdcedfaf467ad5997ad8813725b77b346ba3
Marco Behnke <marco@behnke.biz>
2013-11-24 14:02:48 +0100

    test header

--- tests/helper/HTMLResultTest.php
+++ tests/helper/HTMLResultTest.php
@@ -9,5 +9,9 @@ class HTMLResultTest extends \PHPUnit_Framework_TestCase {
 	public function testResult() {
 		$result = new HTMLResult('test');
 		$this->assertEquals('test', $result->getResult());
+		$this->assertEquals(array(
+            'Content-type: text/html; charset=UTF-8',
+            'Content-length: 4'
+        ), $result->getHeaders());
 	}
 }
--- tests/helper/JSONResultTest.php
+++ tests/helper/JSONResultTest.php
@@ -9,5 +9,9 @@ class JSONResultTest extends \PHPUnit_Framework_TestCase {
 	public function testResult() {
 		$result = new JSONResult(array('test' => 'test'));
 		$this->assertEquals('{"test":"test"}', $result->getResult());
+		$this->assertEquals(array(
+			'Content-type: application/json; charset=UTF-8',
+			'Content-length: 15'
+		), $result->getHeaders());
 	}
 }
