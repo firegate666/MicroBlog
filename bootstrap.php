a17f930215835c30c359cfd9aa5ea3835189b039
Marco Behnke <marco@behnke.biz>
2013-11-25 09:00:56 +0100

    introduce constant for dir seperator

--- bootstrap.php
+++ bootstrap.php
@@ -3,23 +3,23 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 define('APP_ROOT', __DIR__);
-define('RUNTIME_DEFAULT', APP_ROOT . '/runtime/');
-define('TEMPLATES_DEFAULT', APP_ROOT . '/templates/');
-define('CONFIGURATION_DEFAULT', APP_ROOT . '/configuration/');
+define('RUNTIME_DEFAULT', APP_ROOT . DIRECTORY_SEPARATOR . 'runtime' . DIRECTORY_SEPARATOR);
+define('TEMPLATES_DEFAULT', APP_ROOT . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR);
+define('CONFIGURATION_DEFAULT', APP_ROOT . DIRECTORY_SEPARATOR . 'configuration' . DIRECTORY_SEPARATOR);
 
 /*
  * register autoloader
  */
 spl_autoload_register(function ($class_name)
 {
-	$file_name = APP_ROOT . '/src/' . str_replace('\\', '/', $class_name) . '.php';
+	$file_name = APP_ROOT . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . str_replace(array('\\', '_'), DIRECTORY_SEPARATOR, $class_name) . '.php';
 	if (file_exists($file_name))
 	{
 		require_once $file_name;
 	}
 });
 
-$composer_autoload = __DIR__ . '/vendor/autoload.php';
+$composer_autoload = __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 if (file_exists($composer_autoload)) {
 	require_once $composer_autoload;
 }
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../bootstrap.php';
-require_once __DIR__ . '/../setup.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'bootstrap.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'setup.php';
 
 /*
  * create request object
