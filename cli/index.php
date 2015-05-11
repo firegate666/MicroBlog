9c0b64c394347f43a90f2590a330c7dcf66fc598
Marco Behnke <marco@behnke.biz>
2015-05-11 13:37:19 +0200

    prepare cli

---
+++ cli/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'bootstrap.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'setup.php';
+
